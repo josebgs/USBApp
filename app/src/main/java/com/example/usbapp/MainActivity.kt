9f3489b2c8999220643ce6ebd2ce5ab3870e51d4
josebgs <jsantos@riis.com>
2022-05-19 10:52:08 -0400

    device deattached handled

--- app/src/main/java/com/example/usbapp/MainActivity.kt
+++ app/src/main/java/com/example/usbapp/MainActivity.kt
@@ -1,28 +1,52 @@
 package com.example.usbapp
 
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbDeviceConnection
+import android.hardware.usb.UsbInterface
 import android.hardware.usb.UsbManager
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.usbapp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private lateinit var usbManager: UsbManager
+    private lateinit var bytes: ByteArray
+    private val TIMEOUT = 0
+    private val forceClaim = true
+    private var connection: UsbDeviceConnection? = null
+    private var intf: UsbInterface? = null
 
+    var usbReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+
+            if (UsbManager.ACTION_USB_DEVICE_DETACHED == intent.action) {
+                connection?.releaseInterface(intf)
+                connection?.close()
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -34,7 +58,25 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
+        }
+
+        registerReceiver(usbReceiver, IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED))
+
+        val usbManager = getSystemService(Context.USB_SERVICE) as UsbManager?
+        val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+
+        device?.apply {
+
+            device.getInterface(0).also { interf ->
+                intf = interf
+                intf?.getEndpoint(0)?.also { endpoint ->
+                    usbManager?.openDevice(device)?.apply {
+                        claimInterface(intf, forceClaim)
+                        //bulkTransfer(endpoint, bytes, bytes.size, TIMEOUT) //do in another thread
+                    }
+                }
+            }
         }
     }
 
